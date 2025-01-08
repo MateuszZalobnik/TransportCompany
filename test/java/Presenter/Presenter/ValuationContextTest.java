6fc69024927e1074d746cb6a9c1b7f863262f829
MateuszZalobnik <matiz12345678901@wp.pl>
2025-01-08 15:59:35 +0100

    test config

---
+++ test/java/Presenter/Presenter/ValuationContextTest.java
@@ -0,0 +1,14 @@
+package Presenter.Presenter;
+
+import org.junit.jupiter.api.Test;
+
+class ValuationContextTest {
+
+    @Test
+    void setStrategy() {
+    }
+
+    @Test
+    void doBusinessLogic() {
+    }
+}
